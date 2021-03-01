97703f543d5083872439b0a0da43ba79fafba3c9
Gerardo4lipi <185923@iberopuebla.mx>
2021-02-28 22:42:24 -0600

    Programa principal (prueba)

---
+++ main.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "libutils.so"
+
+using namespace std;
+
+int accionUsuario, flagAccionUsuario = 0;
+string origen, destino;
+vector <TBoleto> boletos;
+
+// ¿Debe ir en la biblioteca?
+bool fn_comparar_ciudades(TBoleto& p){
+    if (p.origen == origen && p.destino == destino)
+        return true;
+    else
+        return false;
+};
+
+int main() {
+    TBoleto boletoA {"Puebla", "Tabasco", {21, Tmes::Febrero, 2021, 14, 30}, 350};
+    boletos.push_back(boletoA);
+    TBoleto boletoB {"CDMX", "Tabasco", {21, Tmes::Febrero, 2021, 14, 30}, 350};
+    boletos.push_back(boletoB);
+
+    while (true){
+        if (flagAccionUsuario == 1)
+            break;
+
+        cout << "Boletos de Autobus" << endl;
+        //cout << "[0] Comprar un boleto" << endl;
+        cout << "[1] Consultar boleto entre dos ciudades" << endl;
+        cout << "[2] Salir" << endl;
+        cout << "¿Qué desea realizar?: ";
+        cin >> accionUsuario;
+
+        switch (accionUsuario) {
+
+            //Agregar una nueva estructura de boleto al vector (preguntar en clase)
+            /*case 0:
+              cout << "Ingrese origen del boleto: ";
+              cin >> nuevo_origen;
+              cout << "Ingrese destino del boleto: ";
+              cin >> nuevo_destino;
+
+              break;*/
+
+            case 1:
+                cout << "Ingrese origen del boleto: ";
+                cin >> origen;
+                cout << "Ingrese destino del boleto: ";
+                cin >> destino;
+
+                bool comparacion = fnbuscar(boletos, fn_comparar_ciudades);
+                if (comparacion)
+                    cout << "El boleto entre estas ciudades ha sido vendido" << endl;
+                else
+                    cout << "Boleto disponible" << endl;
+                break;
+
+            case 2:
+                cout << "Gracias";
+                flagAccionUsuario = 1;
+                break;
+
+            default:
+                cout << "Eliga otra opción";
+                break;
+        }
+
+    }
+
+    return 0;
+}
